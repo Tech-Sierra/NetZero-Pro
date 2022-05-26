8ae229efcbb5b8ebcc7524397d0ce426229031aa
Mayur-TechSierra <mayur.arde@techsierra.in>
2022-05-26 18:43:24 +0530

    governance folder in pages

---
+++ NetZero/src/pages/governance-sub-component/Womens.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Womens() {
+  return (
+    <div>
+      <div className="header_title"></div>
+      <div className="bar_chart"></div>
+      <div className="action_button"></div>
+    </div>
+  );
+}
+
+export default Womens;
