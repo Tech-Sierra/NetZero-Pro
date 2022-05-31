c06565e90e62db2b88bb78277fdb883f21cce3bd
Mayur-TechSierra <mayur.arde@techsierra.in>
2022-05-31 11:01:25 +0530

    new hiring by age chart added

--- src/pages/SocialDashboard.js
+++ src/pages/SocialDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MalesFemales from "./social-sub-components/MalesFemales";
+import NewHiringAge from "./social-sub-components/NewHiringAge";
 import TrainingDevelopment from "./social-sub-components/TrainingDevelopment";
 import "./socialDashboard.css";
 
@@ -8,8 +9,7 @@ function SocialDashboard() {
     <div className="social_container">
       <MalesFemales></MalesFemales>
       <TrainingDevelopment></TrainingDevelopment>
-      <MalesFemales></MalesFemales>
-      <TrainingDevelopment></TrainingDevelopment>
+      <NewHiringAge></NewHiringAge>
     </div>
   );
 }
---
+++ src/pages/social-sub-components/NewHiringAge.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { AiFillInfoCircle } from "react-icons/ai";
+import { Button } from "@mui/material";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import { Pie } from "react-chartjs-2";
+import ActionButton from "../governance-sub-component/ActionButton";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function NewHiringAge() {
+  const labels = ["< 30", "30-50", "> 50"];
+  const data = {
+    labels,
+    datasets: [
+      {
+        backgroundColor: ["#32E7A4", "#64B5FC", "#FCEA64"],
+        data: [100005, 90058, 9934],
+        barThickness: 20,
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const option = {
+    plugins: {
+      datalabels: {
+        display: true,
+        color: "black",
+      },
+    },
+  };
+  return (
+    <div className="chart_container pie_two">
+      <div className="header_title">
+        <div className="title">
+          <p>New Hiring in the company in the following age groups</p>
+        </div>
+        <Button>
+          <AiFillInfoCircle color="grey"></AiFillInfoCircle>
+        </Button>
+      </div>
+      <div className="bar_chart">
+        <Pie data={data} option={option}></Pie>
+      </div>
+      <div className="action_button">
+        <ActionButton></ActionButton>
+      </div>
+    </div>
+  );
+}
+
+export default NewHiringAge;
